27c17b5eb1df8706dd790afbeb949d422027598b
KomoriWu <tmwuzhen@qq.com>
2017-08-31 11:36:52 +0800

    flowable

--- app/src/main/java/com/txtled/rxjava2/MainActivity.java
+++ app/src/main/java/com/txtled/rxjava2/MainActivity.java
@@ -1,11 +1,14 @@
 package com.txtled.rxjava2;
 
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Flowable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Flowable
+//        Flowable.just("Hello").subscribe(new Consumer<String>() {
+//            @Override
+//            public void accept(String s) throws Exception {
+//                Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        Flowable.timer(3, TimeUnit.SECONDS).
+//                observeOn(AndroidSchedulers.mainThread()).
+//                subscribe(new Consumer<Long>() {
+//                    @Override
+//                    public void accept(Long aLong) throws Exception {
+//                        Toast.makeText(MainActivity.this, "yanshi", Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+
+
     }
+
 }
